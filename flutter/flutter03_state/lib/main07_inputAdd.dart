5c6abe1a1c395fe6538a554636e96d72b19c1c83
tdchoi <ctd2580@naver.com>
2025-05-19 10:08:56 +0900

    check

---
+++ flutter/flutter03_state/lib/main07_inputAdd.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+// 문 : dialog에서 이름을 넣으면 name배열에 이름 추가하기
+
+void main() {
+  runApp(
+      MaterialApp(
+          home: MyApp()
+      )
+  );
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var name = ['홍길동', '더조은', '빛나리'];
+
+  addName(inputName) {
+    setState(() {
+      name.add(inputName);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+          child: Text('dialog'),
+          onPressed: (){
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return CustomDialog(addName : addName);
+                }
+            );
+          }
+      ),
+      appBar: AppBar(
+        backgroundColor: Color(0xFFf3edf7),
+        leading: Icon(Icons.list),
+        title: Text('주소록'),
+        actions: [Icon(Icons.search), Icon(Icons.share)],
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, i) {
+          return ListTile(
+            leading: Image.asset('assets/user${i+1}.png'),
+            title: Text(name[i]),
+          );
+        },
+        itemCount: name.length,
+      ),
+      bottomNavigationBar: CustomBottom(),
+    );
+  }
+}
+
+class CustomDialog extends StatelessWidget {
+  CustomDialog({super.key, this.addName});
+  final addName;
+  var inputData = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: SizedBox(
+        width: 300,
+        height: 300,
+        child: Column(
+          children: [
+            TextField(onChanged: (text){ inputData = text; },),
+            TextButton(
+                onPressed: (){
+                  addName(inputData);
+                  Navigator.pop(context);
+                },
+                child: Text('완료')),
+            TextButton(
+                onPressed: (){
+                  Navigator.pop(context);
+                },
+                child: Text('취소'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class CustomBottom extends StatelessWidget {
+  const CustomBottom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Icon(Icons.phone),
+          Icon(Icons.article_outlined),
+          Icon(Icons.contacts)
+        ],
+      ),
+    );
+  }
+}
